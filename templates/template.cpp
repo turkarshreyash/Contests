a379dd41ec47b479d7de130cb5a96f7d1da41ec8
turkarshreyash <shturkar90@gmail.com>
2020-10-04 21:23:02 +0530

    Created a template

---
+++ templates/template.cpp
@@ -0,0 +1,36 @@
+/*
+Shreyash H. Turkar
+*/
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<map>
+#include<unordered_set>
+#include<set>
+#include<algorithm>
+#include<utility>
+
+//#define SINGLE_TEST_CASE
+
+#define ll long long
+
+
+void solve(){
+
+
+
+
+
+}
+
+int main(int argc, char *args[]){
+	#ifdef SINGLE_TEST_CASE
+	solve();
+	#else
+	ll t;
+	std::cin>>t;
+	while(t--){
+		solve();
+	}	
+	#endif
+}
