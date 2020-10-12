8d53cdabb3a611035a71fde2c563bb6299d26fe9
turkarshreyash <shturkar90@gmail.com>
2020-10-12 19:39:12 +0530

    add read, sort and print functions for vector<ll>

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,21 +1,80 @@
 /*
 Shreyash H. Turkar
 */
-#include<iostream>
-#include<vector>
-#include<unordered_map>
-#include<map>
-#include<unordered_set>
-#include<set>
-#include<algorithm>
-#include<utility>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <climits>
+#include <iterator>
+using namespace std;
+
+#define ll long long
+#define INF LLONG_MAX
+#define NEG_INF LLONG_MIN
 
 //#define SINGLE_TEST_CASE
 
-#define ll long long
+/*
+
+1) 1 <= N <= 10^3  O(n^2) 
+2) 1 <= N <= 10^5  O(nlogn)
+3) 1 <= N <= 10^8  O(n)
+
+*/
+
+void sort(vector<ll> &arr)
+{
+	sort(arr.begin(), arr.end());
+}
+
+void sortDesc(vector<ll> &arr)
+{
+	sort(arr.begin(), arr.end(), greater<int>());
+}
+
+void read(ll &a){
+	cin>>a;
+}
+
+void read(vector<ll> &arr)
+{
+	for (int i = 0; i < arr.size(); i++)
+	{
+		cin >> arr[i];
+	}
+}
+
+void print(vector<ll> arr)
+{
+	for (ll i : arr)
+	{
+		cout << i << " ";
+	}
+	cout << "\n";
+}
+
+void solve()
+{
+
+	ll n;
+	read(n);
+	vector<ll> arr(n);
+	read(arr);
+	sort(arr);
+	print(arr);
+	sortDesc(arr);
+	print(arr);
 
 
-void solve(){
+
+
+
 
 
 
@@ -23,14 +82,17 @@ void solve(){
 
 }
 
-int main(int argc, char *args[]){
-	#ifdef SINGLE_TEST_CASE
+int main(int argc, char *args[])
+{
+#ifdef SINGLE_TEST_CASE
 	solve();
-	#else
+#else
 	ll t;
-	std::cin>>t;
-	while(t--){
+	std::cin >> t;
+	while (t--)
+	{
 		solve();
-	}	
-	#endif
+		cout << "\n";
+	}
+#endif
 }
